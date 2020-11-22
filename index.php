ba604830d99776024fd68d184f6dbc489c6b075c
Sameer Soni <sameersoni389@gmail.com>
2020-11-22 23:17:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Onine Exam</title>
+<title>Online Exam</title>
 <link rel = "icon" href = "photo.png" type = "image/x-icon"> 
 <link  rel="stylesheet" href="css/bootstrap.min.css"/>
  <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
